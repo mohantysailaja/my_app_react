c976a574972fff8fc8887bdd6ae09caa0d9b889b
sailaja <mohantysailaja@gmail.com>
2023-04-02 22:48:43 +0100

    added button

---
+++ src/myjavascript.js
@@ -0,0 +1,16 @@
+
+//input
+//var username = prompt("Give me your username");
+
+//processing
+//var welcomemessage = "this scrips looks correct to me !!!" + username;
+
+//output
+//alert(welcomemessage);
+
+
+
+var btnclick =document.querySelector("#btnclick");
+btnclick.addEventListener("click",function clickEventHandler(){
+    console.log("clicked")
+})

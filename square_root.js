675eaaeefa8d4bf3ab21c87cdca2af02c4d07c2b
sailaja <mohantysailaja@gmail.com>
2023-04-15 23:45:40 +0100

    added square root and square  of a number in js

--- square_root.js
+++ square_root.js
@@ -2,6 +2,11 @@ function sqaureroot(num){
    // let num;
     var result = Math.sqrt(num);
     console.log("Square root of " + num +" is" ,result);
+    
+    //find the square of a number 
+    var result1 = num * num;
+    console.log("Square of  " + num +" is" ,result1);
+
 }
 sqaureroot(100);
 

0ea7c532702494930dcbac15d8469cb307273750
sailaja <mohantysailaja@gmail.com>
2023-04-26 11:08:46 +0100

    checked let var and const in javacsript

--- letvarconst.js
+++ letvarconst.js
@@ -6,9 +6,19 @@ if(x === 1 ){
 }
 console.log(x);//output expected:1
 //for loop 
+
 for(var i = 0; i<=10; i++)
 {
     console.log("the number is :" + i);
 }
 
+/*code for printing in p id demo using html
+let text = "";
+
+for (let i = 0; i < 5; i++) {
+  text += "The number is " + i + "<br>";
+}
+
+document.getElementById("demo").innerHTML = text;
+*/
 

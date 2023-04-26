8eb02e6d7a63ece1837473d7d84f06943051465c
sailaja <mohantysailaja@gmail.com>
2023-04-26 11:06:32 +0100

    checked let var and const in javacsript

---
+++ letvarconst.js
@@ -0,0 +1,14 @@
+let x = 1;
+
+if(x === 1 ){
+    let x = 2;
+    console.log(x);//output expected:2
+}
+console.log(x);//output expected:1
+//for loop 
+for(var i = 0; i<=10; i++)
+{
+    console.log("the number is :" + i);
+}
+
+

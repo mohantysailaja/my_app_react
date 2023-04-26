5a5042041edf82d6f742174a7d630b85273de983
sailaja <mohantysailaja@gmail.com>
2023-04-26 10:45:06 +0100

    learning arrow function in javacsript

---
+++ arrowfunction.js
@@ -0,0 +1,6 @@
+let myarrowFunction = (a,b) => a * b;
+
+myarrowFunction(3,4);
+console.log(myarrowFunction(3,4));
+//if you have to print the result in HTML page
+//document.getElementById("demo").innerHTML = myFunction(4, 5);
